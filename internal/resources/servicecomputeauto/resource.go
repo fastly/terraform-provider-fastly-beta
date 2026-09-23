4bd0f0e64e1aca7cd28426bc333629d79594a864
Richard Carillo <77027245+rcaril@users.noreply.github.com>
2026-09-23 15:12:47 -0400

    [CDTOOL-1762] Ensure that import marker is only cleared after a read operation (#141)
    * fix(compute): ensure that import marker is only cleared after a successful read
    
    * (fix): update importedBytes order

--- internal/resources/servicecomputeauto/resource.go
+++ internal/resources/servicecomputeauto/resource.go
@@ -805,16 +805,6 @@ func (r *Resource) Read(ctx context.Context, req resource.ReadRequest, resp *res
 	}
 	state.ManagedVersion = types.Int64Value(int64(readVersion))
 
-	importedBytes, diags := req.Private.GetKey(ctx, computePackageImportedPrivateKey)
-	resp.Diagnostics.Append(diags...)
-	if resp.Diagnostics.HasError() {
-		return
-	}
-	imported := len(importedBytes) > 0
-	if imported {
-		resp.Diagnostics.Append(resp.Private.SetKey(ctx, computePackageImportedPrivateKey, nil)...)
-	}
-
 	domains, err := domain.ReadForVersion(ctx, r.providerData.AutoClient(), state.ID.ValueString(), readVersion)
 	if err != nil {
 		resp.Diagnostics.AddError("Error reading service domains", err.Error())
@@ -1009,12 +999,23 @@ func (r *Resource) Read(ctx context.Context, req resource.ReadRequest, resp *res
 	}
 	state.ResourceLink = resourcelink.MatchOrder(resourceLinks, state.ResourceLink)
 
+	importedBytes, diags := req.Private.GetKey(ctx, computePackageImportedPrivateKey)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	imported := len(importedBytes) > 0
+
 	packages, err := computepackage.ReadForVersion(ctx, r.providerData.AutoClient(), state.ID.ValueString(), readVersion, state.Package, imported)
 	if err != nil {
 		resp.Diagnostics.AddError("Error reading Compute package", err.Error())
 		return
 	}
 	state.Package = packages
+
+	if imported {
+		resp.Diagnostics.Append(resp.Private.SetKey(ctx, computePackageImportedPrivateKey, nil)...)
+	}
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
 }
